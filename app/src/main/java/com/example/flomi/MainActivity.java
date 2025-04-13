347342f161604f46b0233b84718f3ba4bec464d7
Cha Juyeon <ch4102109@gmail.com>
2025-04-13 21:23:55 +0900

    first commit

---
+++ app/src/main/java/com/example/flomi/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.flomi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // 1초 후 화면 전환
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, Survey1.class);
+            startActivity(intent);
+            finish(); // 현재 액티비티 종료 (선택사항)
+        }, 1000); // 1000밀리초 = 1초
+    }
+}
